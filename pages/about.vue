cbe65858f8f8a7ae61da8324d88da5d6896a85f3
rewanhamed22 <rewanhamed3@gmail.com>
2024-05-13 14:05:26 +0300

    add products page

---
+++ pages/about.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+      <h2>About</h2>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui dicta
+        numquam voluptatem quo aliquam perspiciatis consequuntur debitis nulla
+        consectetur voluptate.
+      </p>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui dicta
+        numquam voluptatem quo aliquam perspiciatis consequuntur debitis nulla
+        consectetur voluptate.
+      </p>
+    </div>
+  </template>
+  
+  <script setup></script>
+  
+  <style scoped>
+  h2 {
+    margin-bottom: 20x;
+    font-size: 36px;
+  }
+  p {
+    margin: 20px 0;
+  }
+  </style>
+  
---
+++ pages/products/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <p>products</p>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped></style>
