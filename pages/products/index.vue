8afef6182771340d1ffbd42ad8604b9c654fbd0d
rewanhamed22 <rewanhamed3@gmail.com>
2024-05-13 18:11:00 +0300

    make components

--- pages/products/index.vue
+++ pages/products/index.vue
@@ -2,8 +2,7 @@
   <div>
     <div class="grid grid-cols-4 gap-5">
       <div v-for="p in products">
-        <NuxtLink :to="`/products/${p.id}`">{{ p.title }}</NuxtLink>
-      </div>
+        <ProductCard :product="p" /></div>
     </div>
   </div>
 </template>
@@ -14,7 +13,6 @@ definePageMeta({
 });
 //fetch the products
 const { data: products } = await useFetch("https://fakestoreapi.com/products");
-console.log(products);
 </script>
 
 <style scoped></style>
