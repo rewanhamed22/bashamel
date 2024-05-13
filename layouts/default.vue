bce21de580017b7eb6d779517d590e9e2ec477d4
rewanhamed22 <rewanhamed3@gmail.com>
2024-05-13 16:46:33 +0300

    add adding tailwind css

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,17 +1,17 @@
 <template>
   <div>
-    <header>
-      <nav>
-        <NuxtLink to="/">Nuxt</NuxtLink>
-        <ul>
+    <header class="shadow-sm bg-white">
+      <nav class="container mx-auto p-4 flex justify-between">
+        <NuxtLink to="/" class="font-bold">Nuxt</NuxtLink>
+        <ul class="flex gap-4">
           <li><NuxtLink to="/">Home</NuxtLink></li>
           <li><NuxtLink to="/about">About</NuxtLink></li>
-          <li><NuxtLink to="/products">Products</NuxtLink></li>
+          <li><NuxtLink to="/products" class="btn">Products</NuxtLink></li>
         </ul>
       </nav>
     </header>
     <!-- output the page content  -->
-    <div>
+    <div class="container mx-auto p-4">
       <slot />
     </div>
   </div>
--- layouts/products.vue
+++ layouts/products.vue
@@ -1,26 +1,25 @@
 <template>
-    <div>
-      <header>
-        <nav>
-          <NuxtLink to="/products">Nuxt</NuxtLink>
-        </nav>
-      </header>
-      <!-- output the page content  -->
-      <div>
-        <slot />
-      </div>
-      <footer>
-        <ul>
-            <li><NuxtLink to="/">Home</NuxtLink></li>
-            <li><NuxtLink to="/about">About</NuxtLink></li>
-            <li><NuxtLink to="/products">Products</NuxtLink></li>
-          </ul>
-      </footer>
+  <div>
+    <header class="shadow-sm bg-white">
+      <nav class="container mx-auto p-4">
+        <NuxtLink to="/products" class="font-bold">Nuxt</NuxtLink>
+      </nav>
+    </header>
+    <!-- output the page content  -->
+    <div class="container mx-auto p-4">
+      <slot />
     </div>
-  </template>
-  <style scoped>
-  .router-link-exact-active {
-    color: #12b488;
-  }
-  </style>
-  
+    <footer class="container mx-auto p-4 flex justify-between border-t-2">
+      <ul class="flex gap-4">
+        <li><NuxtLink to="/">Home</NuxtLink></li>
+        <li><NuxtLink to="/about">About</NuxtLink></li>
+        <li><NuxtLink to="/products">Products</NuxtLink></li>
+      </ul>
+    </footer>
+  </div>
+</template>
+<style scoped>
+.router-link-exact-active {
+  color: #12b488;
+}
+</style>
